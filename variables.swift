fbb86f7c71b428c54964e85dc8f724672f4fab1b
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-24 09:03:50 +0530

    variables in swift

---
+++ variables.swift
@@ -0,0 +1,10 @@
+/*
+  1. Variables and constants are used to keep track of data in your app.
+  2. Use the "var" and "let" keywords to declare new variables and 
+     constants.
+  3. Use the equal sign to assign data to a variable or constant.
+  4. Use Camel Case as a best practice for naming your variables and
+     constants.
+  5. Constants are like variables but you can't reassign data to them
+     after the initial assignment.
+*/
