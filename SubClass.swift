39de226c0e88761e656ed27d00f4ed06fad23092
React Native Developer <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-26 12:51:28 +0530

    sub classs in swift

--- SubClass.swift
+++ SubClass.swift
@@ -26,3 +26,41 @@ m.name = "kate"
 m.salary = 4000
 m.role = "Super Senior Manager"
 m.doWork()
+
+
+
+
+//example - 2
+class Employee {
+    var name: String = "Preeti"
+    var salary: Int = 1000
+    var role: String = "Manage"
+    
+    func doWork() {
+        print("i am \(name) and i am a \(role)")
+    }
+}
+
+var e: Employee = Employee()
+e.name = "Karuna"
+e.salary = 4000
+e.role = "Senior manager"
+e.doWork()
+
+
+
+class Manager: Employee {
+    var teamSize = 10
+    
+    func checkTeamSize() {
+        print("your team size is \(teamSize)")
+    }
+}
+
+var m: Manager = Manager()
+m.name = "kate"
+m.salary = 4000
+m.role = "Super Senior Manager"
+m.teamSize = 20
+m.doWork()
+m.checkTeamSize()
