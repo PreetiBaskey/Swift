bf23f5697d04bf58e576da70d7084fcc27fdbfce
React Native Developer <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-28 08:26:02 +0530

    Update SubClass.swift

--- SubClass.swift
+++ SubClass.swift
@@ -103,3 +103,41 @@ m.salary = 4000
 m.role = "Super Senior Manager"
 m.teamSize = 20
 m.doWork()
+
+
+
+
+
+//example 4 - super use
+class Employee {
+    var name: String = "Preeti"
+    var salary: Int = 1000
+    var role: String = "Software Developer"
+    
+    func doWork() {
+        print("\(name) do work")
+    }
+}
+
+var e: Employee = Employee()
+e.name = "Sumitra"
+e.salary = 2000
+e.role = "Manager"
+e.doWork()
+
+class Manager: Employee {
+    override func doWork() {
+        super.doWork()
+        salary *= 2
+        print("they are very good worker and now there salary is \(salary)")
+    }
+    
+    func fireEmployee() {
+        print("i can fire employee!")
+    }
+}
+
+var m: Manager = Manager()
+m.doWork()
+m.fireEmployee()
+
